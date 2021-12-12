13039f8e8afc2ea0cf80e04a3bdf870139c704d6
Patrik Olsson <patrik.m.olsson@gmail.com>
2021-12-12 10:21:26 +0100

    Day 10/12

---
+++ Day10/Program.cs
@@ -0,0 +1,140 @@
+﻿using System.Globalization;
+
+// var input = File.ReadAllLines("10/test.txt");
+var input = File.ReadAllLines("10/input.txt");
+
+PartTwo();
+
+void PartTwo()
+{    var points = new List<long>();
+     foreach (var line in input)
+     {
+         var stack = new Stack<char>();
+         var corrupted = false;
+
+         foreach (var c in line.ToCharArray())
+         {
+             switch (c)
+             {
+                 case '[':
+                 case '(':
+                 case '<':
+                 case '{':
+                     stack.Push(c);
+                     break;
+                 case ']':
+                 case ')':
+                 case '>':
+                 case '}':
+                     if (!stack.TryPop(out var s) || !IsMatch(s, c))
+                     {
+                         corrupted = true;
+                     }
+
+                     break;
+             }
+         }
+
+         if (corrupted)
+         {
+             continue;
+         }
+
+         long p = 0;
+         foreach (var c in stack)
+         {
+             var pointForChar = CharToPointsTwo(Invert(c));
+
+             p *= 5;
+             p += pointForChar;
+         }
+
+         points.Add(p);
+     }
+
+     points = points.OrderBy(s => s).ToList();
+
+     Console.WriteLine(points[points.Count / 2]);
+
+}
+
+void PartOne()
+{
+    var points = 0;
+    foreach (var line in input)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var c in line.ToCharArray())
+        {
+            switch (c)
+            {
+                case '[':
+                case '(':
+                case '<':
+                case '{':
+                    stack.Push(c);
+                    break;
+                case ']':
+                case ')':
+                case '>':
+                case '}':
+                    if (!stack.TryPop(out var s) || !IsMatch(s, c))
+                    {
+                        points += CharToPoints(c);
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    Console.WriteLine(points);
+}
+
+char Invert(char c)
+{
+    return c switch
+    {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+    };
+}
+int CharToPointsTwo(char c)
+{
+    return c switch
+    {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+    };
+}
+
+int CharToPoints(char c)
+{
+    return c switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+    };
+}
+
+bool IsMatch(char c1, char c2)
+{
+    return c1 switch
+    {
+        '(' => c2 == ')',
+        '[' => c2 == ']',
+        '{' => c2 == '}',
+        '<' => c2 == '>',
+        _ => throw new ArgumentOutOfRangeException(nameof(c1), c1, null)
+    };
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,136 @@
+﻿// var input = File.ReadAllLines("12/test.txt");
+
+var input = File.ReadAllLines("12/input.txt");
+
+PartOne();
+PartTwo();
+
+void PartOne()
+{
+    var laxVisitedSmallPolicy = false;
+    Execute(laxVisitedSmallPolicy);
+}
+
+void PartTwo()
+{
+    var laxVisitedSmallPolicy = true;
+    Execute(laxVisitedSmallPolicy);
+}
+
+void Execute(bool laxVisitedSmallPolicy)
+{
+    var set = new Dictionary<string, Node>();
+    foreach (var line in input)
+    {
+        var nodes = line.Split('-');
+
+        var contentOne = nodes[0];
+        var contentTwo = nodes[1];
+
+        if (!set.TryGetValue(contentOne, out var nodeOne))
+        {
+            nodeOne = Factory(contentOne, laxVisitedSmallPolicy);
+            set.Add(contentOne, nodeOne);
+        }
+
+        if (!set.TryGetValue(contentTwo, out var nodeTwo))
+        {
+            nodeTwo = Factory(contentTwo, laxVisitedSmallPolicy);
+            set.Add(contentTwo, nodeTwo);
+        }
+
+        nodeOne.Edges.Add(nodeTwo);
+        nodeTwo.Edges.Add(nodeOne);
+    }
+
+    var start = set["start"];
+    Console.WriteLine(start.NumberOfPaths(new HashSet<Node>(), false));
+}
+
+static Node Factory(string input, bool laxVisitedSmallPolicy)
+{
+    return input switch
+    {
+        "end" => new End(input),
+        "start" => new Start(input),
+        _ => new Cave(input, laxVisitedSmallPolicy)
+    };
+}
+
+internal abstract class Node
+{
+    public Node(string name) => this.Name = name;
+
+    public string Name { get; }
+
+    public ISet<Node> Edges { get; } = new HashSet<Node>();
+
+    public abstract int NumberOfPaths(ISet<Node> visitedNodes, bool visitedSmallTwice);
+}
+
+internal class Start : Node
+{
+    public Start(string name) : base(name)
+    {
+    }
+
+    public override int NumberOfPaths(ISet<Node> visitedNodes, bool visitedSmallTwice)
+    {
+        if (visitedNodes.Contains(this))
+        {
+            return 0;
+        }
+
+        var nodes = new HashSet<Node>(visitedNodes) {this};
+
+        return this.Edges
+            .Select(edge => edge.NumberOfPaths(nodes, visitedSmallTwice)).Sum();
+    }
+}
+
+internal class End : Node
+{
+    public End(string name) : base(name)
+    {
+    }
+
+    public override int NumberOfPaths(ISet<Node> visitedNodes, bool visitedSmallTwice) => 1;
+}
+
+internal class Cave : Node
+{
+    private readonly bool laxVisitedSmallPolicy;
+
+    public Cave(string name, bool laxVisitedSmallPolicy) : base(name)
+    {
+        this.laxVisitedSmallPolicy = laxVisitedSmallPolicy;
+        this.IsBig = string.Equals(name, name.ToUpperInvariant(), StringComparison.Ordinal);
+    }
+
+    private bool IsBig { get; }
+
+    public override int NumberOfPaths(ISet<Node> visitedNodes, bool visitedSmallTwice)
+    {
+        var newVisitedSmallTwice = visitedSmallTwice;
+        if (visitedNodes.Contains(this) && !this.IsBig)
+        {
+            if (!this.laxVisitedSmallPolicy)
+            {
+                return 0;
+            }
+
+            if (visitedSmallTwice)
+            {
+                return 0;
+            }
+
+            newVisitedSmallTwice = true;
+        }
+
+        var nodes = new HashSet<Node>(visitedNodes) {this};
+
+
+        return this.Edges
+            .Select(edge => edge.NumberOfPaths(nodes, newVisitedSmallTwice)).Sum();
+    }
+}
