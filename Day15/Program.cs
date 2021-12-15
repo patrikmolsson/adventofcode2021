5b3be99e3294cb1ae61d06d0c9e33bafeb5943dd
Patrik Olsson <patrik.olsson@hm.com>
2021-12-15 17:04:46 +0100

    Re-add part one

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -3,74 +3,95 @@
 // var input = File.ReadAllLines("15/test.txt");
 var input = File.ReadAllLines("15/input.txt");
 
-var width = input.Length;
-var board = new Dictionary<Coord, Node>();
+PartOne();
+PartTwo();
 
-var sorted = new SortedSet<Node>();
-for (var multiplierRow = 0; multiplierRow < 5; multiplierRow++)
-for (var multiplierCol = 0; multiplierCol < 5; multiplierCol++)
-for (var rowIndex = 0; rowIndex < input.Length; rowIndex++)
+void PartOne()
 {
-    Debug.Assert(input.Length == input[rowIndex].Length, "Should have square");
-    for (var colIndex = 0; colIndex < input[rowIndex].Length; colIndex++)
+    var board = GenerateBoard(1);
+    SolveMinPath(board);
+}
+void PartTwo()
+{
+    var board = GenerateBoard(5);
+    SolveMinPath(board);
+}
+
+IDictionary<Coord, Node> GenerateBoard(int repeatCount)
+{
+    var width = input.Length;
+    var board = new Dictionary<Coord, Node>();
+
+    for (var multiplierRow = 0; multiplierRow < repeatCount; multiplierRow++)
+    for (var multiplierCol = 0; multiplierCol < repeatCount; multiplierCol++)
+    for (var rowIndex = 0; rowIndex < input.Length; rowIndex++)
     {
-        var baseRisk = int.Parse(input[rowIndex][colIndex].ToString());
-
-        var newRisk = baseRisk + multiplierCol + multiplierRow;
-
-        if (newRisk > 9)
+        Debug.Assert(input.Length == input[rowIndex].Length, "Should have square");
+        for (var colIndex = 0; colIndex < input[rowIndex].Length; colIndex++)
         {
-            newRisk %= 9;
-        }
+            var baseRisk = int.Parse(input[rowIndex][colIndex].ToString());
 
-        var newRow = rowIndex + (width * multiplierRow);
-        var newCol = colIndex + (width * multiplierCol);
-        var coord = new Coord(newRow, newCol);
-        var node = new Node(coord, newRow == 0 && newCol == 0 ? 0 : int.MaxValue, newRisk);
-        board[coord] = node;
-        sorted.Add(node);
+            var newRisk = baseRisk + multiplierCol + multiplierRow;
+
+            if (newRisk > 9)
+            {
+                newRisk %= 9;
+            }
+
+            var newRow = rowIndex + (width * multiplierRow);
+            var newCol = colIndex + (width * multiplierCol);
+            var coord = new Coord(newRow, newCol);
+            var node = new Node(coord, newRow == 0 && newCol == 0 ? 0 : int.MaxValue, newRisk);
+            board[coord] = node;
+        }
     }
+
+    return board;
 }
 
 
-var endIndex = Convert.ToInt32(Math.Sqrt(board.Count)) - 1;
-var endCoord = new Coord(endIndex, endIndex);
-var end = board[endCoord];
-var start = board[new Coord(0, 0)];
-sorted.Add(start);
-
-do
+void SolveMinPath(IDictionary<Coord, Node> board)
 {
-    var current = sorted.First();
-    sorted.Remove(current);
+    var endIndex = Convert.ToInt32(Math.Sqrt(board.Count)) - 1;
+    var endCoord = new Coord(endIndex, endIndex);
+    var end = board[endCoord];
+    var start = board[new Coord(0, 0)];
+    var sorted = new SortedSet<Node> {start};
 
-    var unvisitedNeighbors = current.Neighbors()
-        .Where(c => board.ContainsKey(c))
-        .Where(c => !board[c].Visited);
-
-    foreach (var neighborCoord in unvisitedNeighbors)
+    do
     {
-        var neighbor = board[neighborCoord];
+        var current = sorted.First();
+        sorted.Remove(current);
 
-        sorted.Remove(neighbor);
+        var unvisitedNeighbors = current.Neighbors()
+            .Where(board.ContainsKey)
+            .Where(c => !board[c].Visited);
 
-        neighbor.TentativeTotalRisk = Math.Min(neighbor.TentativeTotalRisk, current.TentativeTotalRisk + neighbor.Risk);
+        foreach (var neighborCoord in unvisitedNeighbors)
+        {
+            var neighbor = board[neighborCoord];
 
-        sorted.Add(neighbor);
-    }
+            sorted.Remove(neighbor);
 
-    current.Visited = true;
+            neighbor.TentativeTotalRisk =
+                Math.Min(neighbor.TentativeTotalRisk, current.TentativeTotalRisk + neighbor.Risk);
 
-    if (current == end)
-    {
-        break;
-    }
-} while (sorted.Any());
+            sorted.Add(neighbor);
+        }
+
+        current.Visited = true;
+
+        if (current == end)
+        {
+            break;
+        }
+    } while (sorted.Any());
 
 
-var minRisk = end.TentativeTotalRisk;
+    var minRisk = end.TentativeTotalRisk;
 
-Console.WriteLine(minRisk);
+    Console.WriteLine(minRisk);
+}
 
 internal record Coord(int Row, int Col);
 
